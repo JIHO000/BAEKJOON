1c7aa486a82edd1f2341f5535cd83818d31e709b
JiHo <rlagh8515@gmail.com>
2022-03-07 16:23:34 +0900

    A+B-8

--- for문/A+B-8.py
+++ for문/A+B-8.py
@@ -3,4 +3,4 @@ n = int(input())
 data = []
 for i in range(n):
     data.append(list(map(int, sys.stdin.readline().split())))
-    print(f"Case #{i +1}:{data[i][0]} + {data[i][1]} = {data[i][0]+data[i][1]} ")
+    print(f"Case #{i +1}: {data[i][0]} + {data[i][1]} = {data[i][0]+data[i][1]} ")
