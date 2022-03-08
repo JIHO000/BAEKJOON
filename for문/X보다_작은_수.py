7546877ea73393d0944228401b51117d3969a9ad
JiHo <rlagh8515@gmail.com>
2022-03-08 09:19:56 +0900

    X 보다 작은 수

--- for문/X보다_작은_수.py
+++ for문/X보다_작은_수.py
@@ -1,11 +1,6 @@
 import sys
-import time
-start = time.time()  # 시작 시간 저장
- 
+
 N, X = map(int, input().split())
-# 작업 코드
-#data = list(map(int, sys.stdin.readline().split()))
-data = list(map(int, input().split()))
+data = list(map(int, sys.stdin.readline().split()))
 for i in [num for num in data if num < X]:
     print(i)
-print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
