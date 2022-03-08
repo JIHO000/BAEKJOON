29e283df693db0681f1fb6c81265c4929859cb38
JiHo <rlagh8515@gmail.com>
2022-03-08 10:52:22 +0900

    A+B-5

--- while문/A+B-5.py
+++ while문/A+B-5.py
@@ -1,1 +1,10 @@
-a
+import time
+start = time.time()  # 시작 시간 저장
+
+A, B = map(int,input().split())
+#while A != B != 0 != A:
+while A != 0 != B:
+    print(A+B)
+    A, B = map(int,input().split())
+    
+print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
