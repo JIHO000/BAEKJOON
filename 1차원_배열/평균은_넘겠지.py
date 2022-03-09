0aaa1c3cdf794c1bfd4467689baee54c432964e2
JiHo <rlagh8515@gmail.com>
2022-03-09 20:20:40 +0900

    평균은_넘겠지.py

--- 1차원_배열/평균은_넘겠지.py
+++ 1차원_배열/평균은_넘겠지.py
@@ -1,4 +1,5 @@
 import sys
-for _ in range(int(input())):
+C = int(input())
+for _ in range(C):
     n, *data = map(int, sys.stdin.readline().split())
     print("%.3f%%" % (len([num for num in data if num > sum(data)/n])/n * 100))
