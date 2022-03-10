6762686e5867016a92eb56b093e14a4cec10f859
JiHo <rlagh8515@gmail.com>
2022-03-10 19:24:19 +0900

    한수

--- 함수/1065.py
+++ 함수/1065.py
@@ -1,1 +1,14 @@
-d
+N = int(input())
+if N < 100:
+    print(N)
+else:
+    result = 99
+    for i in range(100, N + 1):
+        X = list(map(int, str(i)))
+        j = 0
+        while X[j] - X[j+1] == X[j+1] - X[j+2]:
+            j += 1
+            if j == len(X) - 2:
+                result += 1
+                break
+    print(result)
