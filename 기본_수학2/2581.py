b967d36453fef768cf7a55b36930e831c2262cdd
JiHo <rlagh8515@gmail.com>
2022-03-14 11:20:24 +0900

    소수

---
+++ 기본_수학2/2581.py
@@ -0,0 +1,16 @@
+N = int(input())
+M = int(input())
+demical_list=[]
+
+for i in range(N,M + 1):
+    if i != 1:
+        for j in range(2, int(i**.5)+1):
+            if i % j == 0:
+                break
+        else:
+            demical_list.append(i)
+if demical_list:
+    print(sum(demical_list))
+    print(demical_list[0])
+else: 
+    print(-1)
