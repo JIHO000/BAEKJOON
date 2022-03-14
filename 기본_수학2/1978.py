e55159e9cdbfbddff7b9fb176ced6d13965b25c6
JiHo <rlagh8515@gmail.com>
2022-03-14 11:08:33 +0900

    소수 찾기

--- 기본_수학2/1978.py
+++ 기본_수학2/1978.py
@@ -1,11 +1,17 @@
 N = int(input())
-data = map(int,input().split())
-cnt = 0
-for i in data:
+M = int(input())
+demical_list=[]
+
+for i in range(N,M + 1):
     if i != 1:
-        for j in range(2, i):
+        for j in range(2, int(i**.5)+1):
             if i % j == 0:
                 break
         else:
-            cnt += 1
-print(cnt)
+            demical_list.append(i)
+if demical_list:
+    print(sum(demical_list))
+    print(demical_list[0])
+else: 
+    print(-1)
+    
