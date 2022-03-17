b1a48fc821cb7e1daa1ac85717a8f1be60cc6152
JiHo <rlagh8515@gmail.com>
2022-03-17 16:51:56 +0900

    덩치

---
+++ 브루트_포스/7568.py
@@ -0,0 +1,12 @@
+N = int(input())
+group = []
+for _ in range(N):
+    *a, =map(int,input().split())
+    group.append(a)
+    print(group)
+for i in group:
+    rank = 1
+    for j in group:
+        rank += 1 if i[0] < j[0] and i[1] < j[1] else 0
+    print(rank, end=" ")
+        
