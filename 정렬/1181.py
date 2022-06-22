2aeedeafc58247cd32d2c2b6039307b7ecf35f3f
jiho <rlagh8515@gmail.com>
2022-06-22 18:30:49 +0900

    단어 정렬

---
+++ 정렬/1181.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+words= []
+for _ in range(n):
+    words.append(sys.stdin.readline().strip())
+words = list(set(words))
+words.sort()
+words.sort(key = len)
+for i in words:
+    print(i)
