51d8b45e707ac740b06359251415217ecaa205e1
jiho <rlagh8515@gmail.com>
2023-06-26 19:32:31 +0900

    10813

--- 1차원_배열/10813.py
+++ 1차원_배열/10813.py
@@ -1,10 +1,13 @@
 N,M = map(int, input().split())
 
 basket = [i for i in range(1,N+1)]
+tmp = []
 
 for i in range(M):
     i,j = map(int, input().split())
-    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
+    tmp = basket[i-1:j]
+    tmp.reverse()
+    basket[i-1:j] =tmp
 
 for i in range(N):
     print(basket[i], end = ' ')
